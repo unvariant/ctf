ddd29ff41c210d38ead0f48ca29334e662781ad0
ninefox <unvariant.winter@gmail.com>
2022-07-03 22:15:00 -0700

    add comments to find.py

--- googlectf-2022/hardware/weather/find.py
+++ googlectf-2022/hardware/weather/find.py
@@ -1,8 +1,13 @@
+# function to check if byte a contains byte b
 def valid(a, b):
     return a | b == a
 
+# 02h is the opcode for LJMP
+# 12h is the opcode for LCALL
 ops = [0x02, 0x12]
 
+# firmware bytes extracted from the eeprom
+# in an earlier program
 f = open("firmware.bin", "rb")
 d = f.read()
 f.close()
@@ -10,6 +15,7 @@ f.close()
 ps = []
 for i in range(len(d)-1):
     b = d[i]
+    # stop when the FFh region is reached
     if d[i:i+3] == b'\xff\xff\xff':
         break
     for op in ops:
